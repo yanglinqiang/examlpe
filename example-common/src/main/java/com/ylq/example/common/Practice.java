2fe83d552cfe6a9004c3b5b7a94aecfceff619de
杨林强 <linqiang.yang@tendcloud.com>
2016-07-23 15:57:18 +0800

    test

--- example-common/src/main/java/com/ylq/example/common/Practice.java
+++ example-common/src/main/java/com/ylq/example/common/Practice.java
@@ -1,11 +1,24 @@
 package com.ylq.example.common;
 
+import java.io.IOException;
+
 /**
  * Class description goes here
  * Created by ylq on 16/4/15 下午12:24.
  */
 public class Practice {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println('C'&'A');
+    public static void main(String[] args) throws InterruptedException, IOException {
+//        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+//        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("sdf");
+//            }
+//        }, 1, 1, TimeUnit.MINUTES);
+//
+//        Thread.sleep(100000000);
+        Integer i=0;
+        Integer j=i++;
+        System.out.println(j);
     }
 }
